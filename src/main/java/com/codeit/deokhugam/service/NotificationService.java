24f4e238c3158d8a38c7f9b40d12516015a5c5da
geonjineer <atoo1520@gmail.com>
2025-10-21 17:38:34 +0900

    feat: CommentService.java 개발을 위한 NotificationService.java 생성

---
+++ src/main/java/com/codeit/deokhugam/service/NotificationService.java
@@ -0,0 +1,10 @@
+package com.codeit.deokhugam.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+
+}

f86451a367b2b2cdd205293a91b63668bf531dfe
geonjineer <atoo1520@gmail.com>
2025-10-21 17:30:47 +0900

    fix: DB 어노테이션 수정

--- src/main/java/com/codeit/deokhugam/domain/entity/Comment.java
+++ src/main/java/com/codeit/deokhugam/domain/entity/Comment.java
@@ -3,22 +3,27 @@ package com.codeit.deokhugam.domain.entity;
 import com.codeit.deokhugam.domain.entity.base.BaseUpdatableEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 
 
 @Entity
 public class Comment extends BaseUpdatableEntity {
 
-  @Column(name = "content", nullable = false)
+  @Column(nullable = false)
   private String content;
-  @Column(name = "deleted", nullable = false)
-  private boolean deleted;
 
-  @OneToOne(orphanRemoval = true)
+  @Column(nullable = false)
+  private boolean deleted = false; //기본값 설정
+
+  // 하나의 리뷰(Review)는 여러 댓글(Comment)을 가짐
+  @ManyToOne(fetch = FetchType.LAZY) // 지연 로딩
   @JoinColumn(name = "review_id", nullable = false)
   private Review review;
-  @OneToOne(orphanRemoval = true)
+
+  // 하나의 사용자(Member)는 여러 댓글(Comment)을 작성함
+  @ManyToOne(fetch = FetchType.LAZY) // 지연 로딩
   @JoinColumn(name = "member_id", nullable = false)
   private Member member;
 }

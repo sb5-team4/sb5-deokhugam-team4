a1b5b6bb7287dba9582abbbd3bde14ba3f2237f6
geonjineer <atoo1520@gmail.com>
2025-10-21 17:37:02 +0900

    feat: CommentService.java 개발을 위한 레포지토리 생성

---
+++ src/main/java/com/codeit/deokhugam/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.codeit.deokhugam.repository;
+
+import com.codeit.deokhugam.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}

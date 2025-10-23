74c8b76b4a25bbf599db5aeb02285c60a331a720
geonjineer <atoo1520@gmail.com>
2025-10-23 16:01:43 +0900

    feat: CommentControllerTest.java 개발 완료

--- src/main/java/com/codeit/deokhugam/dto/request/CommentCreateRequest.java
+++ src/main/java/com/codeit/deokhugam/dto/request/CommentCreateRequest.java
@@ -3,12 +3,16 @@ package com.codeit.deokhugam.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentCreateRequest {
 
   @NotNull(message = "리뷰 ID는 필수입니다.")

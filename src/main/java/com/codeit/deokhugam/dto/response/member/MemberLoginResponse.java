2db74e979abc03a81f1a8eeeeea34e39d363d341
SeoChanGyu <sdopre12@gmail.com>
2025-10-24 15:02:15 +0900

    chore: dto들 파일위치변경

--- src/main/java/com/codeit/deokhugam/dto/response/member/MemberLoginResponse.java
+++ src/main/java/com/codeit/deokhugam/dto/response/member/MemberLoginResponse.java
@@ -1,8 +1,8 @@
-package com.codeit.deokhugam.dto.response;
+package com.codeit.deokhugam.dto.response.member;
 
 import java.time.Instant;
 
-public record MemberCreatedResponse(
+public record MemberLoginResponse(
     Long id,
     String email,
     String nickname,

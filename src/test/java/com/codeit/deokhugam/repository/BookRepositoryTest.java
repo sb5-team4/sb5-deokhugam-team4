886bfb6b7e1e40a3277795ab8ff221ebf72e5a4d
kjn4101 <wldls3866@gmail.com>
2025-10-28 15:23:06 +0900

    fix: BookRepositoryTest에 @@EnableJpaAuditing 추가

--- src/test/java/com/codeit/deokhugam/repository/BookRepositoryTest.java
+++ src/test/java/com/codeit/deokhugam/repository/BookRepositoryTest.java
@@ -12,8 +12,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @DataJpaTest
+@EnableJpaAuditing
 class BookRepositoryTest extends DataBaseConnectionSupport {
 
   @Autowired

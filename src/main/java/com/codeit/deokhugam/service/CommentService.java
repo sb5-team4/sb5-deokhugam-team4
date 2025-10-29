29b9736ed23bd83188a20350cf786e54c5b61a01
geonjineer <atoo1520@gmail.com>
2025-10-29 18:12:26 +0900

    feat: 댓글 목록 조회 service 완료

--- src/main/java/com/codeit/deokhugam/service/CommentService.java
+++ src/main/java/com/codeit/deokhugam/service/CommentService.java
@@ -5,12 +5,17 @@ import com.codeit.deokhugam.common.exception.ResourceNotFoundException;
 import com.codeit.deokhugam.domain.entity.Comment;
 import com.codeit.deokhugam.domain.entity.Member;
 import com.codeit.deokhugam.domain.entity.Review;
-import com.codeit.deokhugam.dto.command.CommentCreateCommand;
-import com.codeit.deokhugam.dto.result.CommentCreateResult;
+import com.codeit.deokhugam.dto.command.comment.CommentCreateCommand;
+import com.codeit.deokhugam.dto.command.comment.CursorPageCommentCommand;
+import com.codeit.deokhugam.dto.response.comment.CommentResponse;
+import com.codeit.deokhugam.dto.result.comment.CommentCreateResult;
+import com.codeit.deokhugam.dto.result.comment.CursorPageCommentResult;
 import com.codeit.deokhugam.mapper.CommentMapper;
 import com.codeit.deokhugam.repository.CommentRepository;
 import com.codeit.deokhugam.repository.MemberRepository;
 import com.codeit.deokhugam.repository.ReviewRepository;
+import java.time.Instant;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -60,6 +65,53 @@ public class CommentService {
 
   // mapper에 있는 Result로 매핑해주는 메서드를 사용해서 Result로 변환
     return commentMapper.toCommentCreateResult(savedComment);
+  }
+
+  //댓글 목록 조회 (커서 기반 페이지네이션 / 무한 스크롤)
+  @Transactional(readOnly = true)
+  public CursorPageCommentResult findCommentsByReviewId(CursorPageCommentCommand command) {
+
+    int limit = command.commentLimit();
+
+    //Repository에서 limit+1개 조회
+    List<Comment> commentList = commentRepository.findByCommentReviewIdWithCursor(
+        command.reviewId(),
+        command.direction(),
+        command.after(),    // 보조 커서 (createdAt)
+        command.cursorId(), // 메인 커서 (id)
+        limit
+    );
+
+    // 다음 페이지 유무 판단
+    boolean hasNext = commentList.size() > limit;
+
+    // 실제 반환할 리스트 (limit 개수만큼만 자르기)
+    List<Comment> commentListAfter = hasNext ? commentList.subList(0, limit) : commentList;
+
+
+    // 다음 커서(nextAfter, nextCursorId) 계산
+    Instant nextAfter = null;
+    Long nextCursorId = null;
+    if (hasNext) {
+      Comment lastComment = commentListAfter.get(limit - 1);
+      nextAfter = lastComment.getCreatedAt();
+      nextCursorId = lastComment.getId();
+    }
+
+    // List<Comment> -> List<CommentResponse> 변환
+    List<CommentResponse> commentListResponse = commentListAfter.stream()
+        .map(commentMapper::toCommentListResponse)
+        .toList();
+
+    // Result 객체 생성 후 반환
+    return new CursorPageCommentResult(
+        commentListResponse,
+        nextCursorId != null ? String.valueOf(nextCursorId) : null, // API 명세서에 맞춰 String 변환
+        nextAfter,
+        commentListResponse.size(),
+        0L,
+        hasNext
+    );
   }
 
 }

7a7133bf54880589ce96233497c3825ba6058aef
geonjineer <atoo1520@gmail.com>
2025-10-29 18:12:14 +0900

    feat: 댓글 목록 조회 controller 완료

--- src/main/java/com/codeit/deokhugam/controller/CommentController.java
+++ src/main/java/com/codeit/deokhugam/controller/CommentController.java
@@ -1,19 +1,25 @@
 package com.codeit.deokhugam.controller;
 
-import com.codeit.deokhugam.dto.command.CommentCreateCommand;
-import com.codeit.deokhugam.dto.request.CommentCreateRequest;
-import com.codeit.deokhugam.dto.response.CommentResponse;
-import com.codeit.deokhugam.dto.result.CommentCreateResult;
+import com.codeit.deokhugam.dto.command.comment.CommentCreateCommand;
+import com.codeit.deokhugam.dto.command.comment.CursorPageCommentCommand;
+import com.codeit.deokhugam.dto.request.comment.CommentCreateRequest;
+import com.codeit.deokhugam.dto.response.comment.CommentResponse;
+import com.codeit.deokhugam.dto.response.comment.CursorPageCommentResponse;
+import com.codeit.deokhugam.dto.result.comment.CommentCreateResult;
+import com.codeit.deokhugam.dto.result.comment.CursorPageCommentResult;
 import com.codeit.deokhugam.mapper.CommentMapper;
 import com.codeit.deokhugam.service.CommentService;
 import jakarta.validation.Valid;
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,12 +30,7 @@ public class CommentController {
   private final CommentService commentService;
   private final CommentMapper commentMapper;
 
-  /**
-   * 댓글 생성 POST
-   * @param requestMemberId
-   * @param request
-   * @return
-   */
+  //댓글 생성 POST
   @PostMapping
   public ResponseEntity<CommentResponse> createComment(
       // 요청자 멤버ID
@@ -50,4 +51,40 @@ public class CommentController {
     // 201 Created 상태 코드와 Response 요청자에게 반환
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
   }
+
+  // 댓글 목록 조회 GET
+  @GetMapping
+  public ResponseEntity<CursorPageCommentResponse> getAllComments(
+      // --- API 명세서 Parameters ---
+      @RequestParam("reviewId")
+      Long reviewId,
+
+      @RequestParam(value = "direction", defaultValue = "DESC")
+      String direction,
+
+      @RequestParam(value = "cursor", required = false)
+      Long cursorId,
+
+      @RequestParam(value = "after", required = false)
+      Instant after,
+
+      @RequestParam(value = "limit", defaultValue = "50")
+      int limit
+  ){
+
+    // Mapper를 통해 파라미터 -> Command 객체로 변환
+    CursorPageCommentCommand command = commentMapper.toCursorPageCommentCommand(
+        reviewId, direction, after, cursorId, limit
+    );
+    // Service 호출
+    CursorPageCommentResult result = commentService.findCommentsByReviewId(command);
+
+    //Mapper를 통해 Result -> Response 변환
+    CursorPageCommentResponse response = commentMapper.toCursorPageCommentResponse(result);
+
+    // 200 OK 상태 코드와 Response 반환
+    return ResponseEntity.ok(response);
+  }
+
+
 }

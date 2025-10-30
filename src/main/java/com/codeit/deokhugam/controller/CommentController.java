8ba15ebe86cf24ccf85805f01170d5cc47708b76
geonjineer <atoo1520@gmail.com>
2025-10-30 11:46:59 +0900

    feat: 댓글 상세 조회 로직 완성

--- src/main/java/com/codeit/deokhugam/controller/CommentController.java
+++ src/main/java/com/codeit/deokhugam/controller/CommentController.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -83,6 +84,15 @@ public class CommentController {
     CursorPageCommentResponse response = commentMapper.toCursorPageCommentResponse(result);
 
     // 200 OK 상태 코드와 Response 반환
+    return ResponseEntity.ok(response);
+  }
+
+  // 댓글 상세 조회
+  @GetMapping("/{commentId}")
+  public ResponseEntity<CommentResponse> getCommentId(
+      @PathVariable("commentId") Long commentId
+  ) {
+    CommentResponse response = commentService.findCommentById(commentId);
     return ResponseEntity.ok(response);
   }
 

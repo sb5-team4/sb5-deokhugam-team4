3fdb90e5d2b6163643d2abf845d9c8cbc0d4ca5b
geonjineer <atoo1520@gmail.com>
2025-10-31 15:45:57 +0900

    fix: book sql과 entity 리뷰카운트 타입 일치

--- src/main/java/com/codeit/deokhugam/domain/entity/Book.java
+++ src/main/java/com/codeit/deokhugam/domain/entity/Book.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;

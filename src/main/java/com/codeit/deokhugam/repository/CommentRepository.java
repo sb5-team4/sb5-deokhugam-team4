3a0b0e6292f47515d6af40a16c9e7b4b64b07ddc
geonjineer <atoo1520@gmail.com>
2025-10-31 13:06:43 +0900

    fix: import 수정

--- src/main/java/com/codeit/deokhugam/repository/CommentRepository.java
+++ src/main/java/com/codeit/deokhugam/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package com.codeit.deokhugam.repository;
 
 import com.codeit.deokhugam.domain.entity.Comment;
 import com.codeit.deokhugam.repository.comment.CommentRepositoryCustom;
+import java.time.Instant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // JpaRepository와 함께 CommentRepositoryCustom 상속

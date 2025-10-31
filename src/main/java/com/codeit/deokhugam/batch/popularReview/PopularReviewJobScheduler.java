fa0913912d61bb4912d6e70d261913ffc2938031
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-10-31 10:37:48 +0900

    fix: 스케줄러 시간 수정

--- src/main/java/com/codeit/deokhugam/batch/popularReview/PopularReviewJobScheduler.java
+++ src/main/java/com/codeit/deokhugam/batch/popularReview/PopularReviewJobScheduler.java
@@ -25,9 +25,9 @@ public class PopularReviewJobScheduler {
   }
 
   //   1분마다 실행 (cron: "0 */1 * * * *")
-  @Scheduled(cron = "0 */1 * * * *", zone = "Asia/Seoul")
-  // 매일 자정 0시 실행
-//  @Scheduled(cron = "0 0 1 * * *", zone = "Asia/Seoul")
+//  @Scheduled(cron = "0 */1 * * * *", zone = "Asia/Seoul")
+  // 매일 새벽 1시 실행
+  @Scheduled(cron = "0 0 1 * * *", zone = "Asia/Seoul")
   public void runPopularReviewJob() {
     try {
       JobParameters jobParameters = new JobParametersBuilder()

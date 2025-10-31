c6271ad264af87190b19167b52933b73221d4ad9
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-10-31 10:03:44 +0900

    fix: PR 수정사항 - 변수명 모호함, JOB 시작마다 멤버변수 초기화

--- src/main/java/com/codeit/deokhugam/batch/popularReview/PopularReviewJobScheduler.java
+++ src/main/java/com/codeit/deokhugam/batch/popularReview/PopularReviewJobScheduler.java
@@ -24,10 +24,10 @@ public class PopularReviewJobScheduler {
     runPopularReviewJob();
   }
 
-  // 1분마다 실행 (cron: "0 */1 * * * *")
-  // @Scheduled(cron = "0 */1 * * * *", zone = "Asia/Seoul")
+  //   1분마다 실행 (cron: "0 */1 * * * *")
+  @Scheduled(cron = "0 */1 * * * *", zone = "Asia/Seoul")
   // 매일 자정 0시 실행
-  @Scheduled(cron = "0 0 1 * * *", zone = "Asia/Seoul")
+//  @Scheduled(cron = "0 0 1 * * *", zone = "Asia/Seoul")
   public void runPopularReviewJob() {
     try {
       JobParameters jobParameters = new JobParametersBuilder()

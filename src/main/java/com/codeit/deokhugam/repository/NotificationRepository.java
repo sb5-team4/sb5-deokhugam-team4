82e23d0c8b18b6b47930662003d59a3ccdeecbde
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-11-03 17:43:27 +0900

    feat : Hard-Delete-batch

--- src/main/java/com/codeit/deokhugam/repository/NotificationRepository.java
+++ src/main/java/com/codeit/deokhugam/repository/NotificationRepository.java
@@ -1,9 +1,13 @@
 package com.codeit.deokhugam.repository;
 
 import com.codeit.deokhugam.domain.entity.Notification;
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository
     extends JpaRepository<Notification, Long>, NotificationQueryRepository {
@@ -11,4 +15,9 @@ public interface NotificationRepository
   Optional<Notification> findByIdAndDeletedIsFalse(Long id);
 
   List<Notification> findByMemberId(Long memberId);
+
+  @Modifying
+  @Query("DELETE FROM Notification n WHERE n.deleted = true AND n.updatedAt < :updatedAt")
+  void hardDeleteAllBefore(@Param("updatedAt") Instant updatedAt);
+
 }

244ee1f986e21472f01d6e48e027efd7d86e4ab5
geonjineer <atoo1520@gmail.com>
2025-11-04 12:23:15 +0900

    feat: 알림 삭제 배치의 Reader가 사용할 쿼리 추가

--- src/main/java/com/codeit/deokhugam/repository/NotificationRepository.java
+++ src/main/java/com/codeit/deokhugam/repository/NotificationRepository.java
@@ -1,9 +1,12 @@
 package com.codeit.deokhugam.repository;
 
 import com.codeit.deokhugam.domain.entity.Notification;
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository
     extends JpaRepository<Notification, Long>, NotificationQueryRepository {
@@ -11,4 +14,11 @@ public interface NotificationRepository
   Optional<Notification> findByIdAndDeletedIsFalse(Long id);
 
   List<Notification> findByMemberId(Long memberId);
+
+  // 알림 삭제 배치의 Reader가 사용할 쿼리
+  @Query(
+      "SELECT n FROM Notification n " +
+          "WHERE n.confirmed = true AND n.updatedAt < :cutoffDate"
+  )
+  List<Notification> findOldConfirmedNotifications(@Param("cutoffDate") Instant cutoffDate);
 }

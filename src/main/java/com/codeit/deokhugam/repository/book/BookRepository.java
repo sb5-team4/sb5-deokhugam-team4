719b5cbbb8fd0308c16022eaec10d023fe0520df
kjn4101 <wldls3866@gmail.com>
2025-11-04 22:03:00 +0900

    feat: 요구 사항에 맞춰 인기 도서 계산 시 논리 삭제된 리뷰 데이터도 포함하여 평점을 계산하도록 수정

--- src/main/java/com/codeit/deokhugam/repository/book/BookRepository.java
+++ src/main/java/com/codeit/deokhugam/repository/book/BookRepository.java
@@ -33,7 +33,6 @@ public interface BookRepository extends JpaRepository<Book, Long>, BookQueryRepo
       LEFT JOIN Review r ON r.book.id = b.id
           AND r.createdAt >= :startDate
           AND r.createdAt < :endDate
-      WHERE b.deleted = false
       GROUP BY b.id, b.title, b.author, b.thumbnailUrl
       HAVING COUNT(r.id) > 0
       ORDER BY (COUNT(r.id) * 0.4 + COALESCE(AVG(CAST(r.rating AS double)), 0.0) * 0.6) DESC
